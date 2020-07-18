f111f2101b0fd0096bfec77109371afc5b93fd87
wookoo <nanayagoon@daum.net>
2020-07-18 20:44:37 +0900

    학생회관 파싱 구현

--- bot/restaurant.py
+++ bot/restaurant.py
@@ -5,20 +5,31 @@ from urllib import parse
 #from bot import button
 from bs4 import BeautifulSoup
 import time
+import re
+
+
 def Search(restaurant):
     data = requests.get('https://www.hknu.ac.kr/kor/176/subview.do')
+    #data = requests.get('https://www.hknu.ac.kr/kor/176/subview.do?enc=Zm5jdDF8QEB8JTJGZGlldCUyRmtvciUyRjIlMkZ2aWV3LmRvJTNGbW9uZGF5JTNEMjAyMC4wMi4yNCUyNndlZWslM0RwcmUlMjY%3D')
     soup = BeautifulSoup(data.text,"html.parser")
     now = time.localtime()
     today = now.tm_wday
     if (today >=5):
         print("없엉")
     today = (0*2)#(today * 2)+1
-
     body = soup.select('#viewForm > table > tbody ')
     body = str(body[0])
     body = body.split("<tr>")[1:]
     for i in range(today,today+4):
-        print(body[i])
-    
+        line = body[i].strip()
+        line = re.sub("<br(/)>","\n",line)
+        line = re.sub("</tr>","",line)
+        line = re.sub("\d+\.\d+\.\d+","",line)
+        line = re.sub("\( (월|화|수|목|금) \)","",line)
+        line = re.sub("<t[d|h] rowspan=\"\d+\">","",line)
+        line = re.sub("<|/|t[d|h]|br|>","",line)
+        line = re.sub("!-- colspan=\"\d+\"--","",line)
+        line = line.strip()
+        print(line,end="\n======================\n")
 
 Search(123)
