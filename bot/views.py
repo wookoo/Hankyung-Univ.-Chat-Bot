a4ab837f6d80c83dbc19bb1354d240914648bf4b
wookoo <nanayagoon@daum.net>
2020-07-18 18:49:18 +0900

    도서 검색 구현

--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from bot import library
+import json
 # Create your views here.
 
 @csrf_exempt
 def message(request):
-    return JsonResponse({"우":"쿠"})
+
+    answer = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(answer)
+    command = return_json_str['userRequest']['utterance']
+    command = command.strip()
+    command = command.replace(" ","")
+    if command == "도서검색":
+        bookName = return_json_str['action']['params']['bookName']
+        return library.Search(bookName)
+
+    return library.Search("몰라")
