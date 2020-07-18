86c2f871fe67a57ab6b9513499c2ed6c7301a3c4
wookoo <nanayagoon@daum.net>
2020-07-18 17:06:10 +0900

    views 생성

--- bot/views.py
+++ bot/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 # Create your views here.
+
+@csrf_exempt
+def message(request):
+    return JsonResponse({"우":"쿠"})
