6980f04fb091df25bcb9a8a891c1be808c20a458
wookoo <nanayagoon@daum.net>
2020-07-18 19:08:27 +0900

    에러메세지 추가

--- bot/views.py
+++ bot/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from bot import library
+from bot import button
 import json
+
 # Create your views here.
 
 @csrf_exempt
@@ -16,5 +18,19 @@ def message(request):
     if command == "도서검색":
         bookName = return_json_str['action']['params']['bookName']
         return library.Search(bookName)
+    else:
 
-    return library.Search("몰라")
+        return JsonResponse({
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "지원하지 않는 명령어에요"
+                }
+            }
+        ],
+        'quickReplies':button.simpleTextquickReplies,
+    },
+
+})
