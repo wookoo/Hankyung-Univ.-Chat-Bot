b564df0b87e744b0e55c4a9978ae609b60f8c32c
wookoo <nanayagoon@daum.net>
2020-07-18 19:08:21 +0900

    버튼 추가

---
+++ bot/button.py
@@ -0,0 +1,10 @@
+quickReplies = [{
+'type': 'text',
+'label': '도서 검색',
+'messageText': '도서 검색'
+           }]
+simpleTextquickReplies = [{
+    'label': '도서 검색',
+    'action': 'message',
+    'messageText': '도서 검색'
+                }]
