2cd659ca0c22aafcbb8d52256e6be34a4909fbbf
wookoo <nanayagoon@daum.net>
2020-07-18 22:08:36 +0900

    버튼 재배치

--- bot/button.py
+++ bot/button.py
@@ -1,10 +1,22 @@
 quickReplies = [{
 'type': 'text',
-'label': '도서 검색',
-'messageText': '도서 검색'
-           }]
-simpleTextquickReplies = [{
+'label': '오늘의 학식',
+'messageText': '오늘의 학식'
+           },
+           {
+           'type': 'text',
+           'label': '도서 검색',
+           'messageText': '도서 검색'
+                      }]
+simpleTextquickReplies = [
+{
+    'label': '오늘의 학식',
+    'action': 'message',
+    'messageText': '오늘의 학식'
+                },
+                {
     'label': '도서 검색',
     'action': 'message',
     'messageText': '도서 검색'
-                }]
+                },
+    ]
