aaa7b2012a02454ea1d23db24ad5d260a48bef24
wookoo <nanayagoon@daum.net>
2020-07-18 17:05:59 +0900

    타임존 설정

--- hkuniv/settings.py
+++ hkuniv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's$1@-go#8h!dg^i-g8l%lbfttq86cm&#p=vgj0gzkq2s@_6acm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
